7b4ab49cf1a94d3599de85acd8ac30729a2a9a8b
aravind <84662766+aravind1217@users.noreply.github.com>
2021-06-07 21:16:30 +0530

    Update settings.py

--- Weather/settings.py
+++ Weather/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zo)-r8d!=0p&68b*53(-5kpe(2r8$b&dk5($q#7)3-c5m0gb!x'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aravindsharma1217@gmail.com'
-EMAIL_HOST_PASSWORD = "mjfsifbgqwxicbuv"
+EMAIL_HOST_USER = 'Your email'
+EMAIL_HOST_PASSWORD = "Your password"
